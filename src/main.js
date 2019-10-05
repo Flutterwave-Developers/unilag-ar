66573039cd04e06ba709e8f1a5a0303b25a70276
Benjamin Alamu: Codeblooded <oluwaseunalamu@gmail.com>
2019-10-05 15:55:30 +0100

    Added Modal

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import router from './router'
 import App from './App.vue'
 import '@/assets/css/tailwind.css'
+import VueSwal from 'vue-swal'
+
+Vue.use(VueSwal)
 
 Vue.config.productionTip = false
 
--- src/views/AddDrug.vue
+++ src/views/AddDrug.vue
@@ -29,7 +29,7 @@
     </div>
   </div>
 
-<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+<button @click.prevent="showAlert" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
   Submit
 </button>
 </form>
@@ -43,6 +43,15 @@ import DrugCard from "@/components/DrugCard.vue";
     export default {
         components:{
             DrugCard
+        },
+        methods: {
+            showAlert() {
+                            this.$swal(
+              "Success!",
+              "New Drug added!",
+              "success"
+            );
+            }
         }
     }
 </script>
